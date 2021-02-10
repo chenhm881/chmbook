e9e52a431c7ea0fa158b3849b6588d25b55f8c26
huiming chen <chenhm881@163.com>
2021-02-10 18:31:31 +0800

    multiple datasource transaction of blog-service

--- blog-service/src/main/java/com/chm/book/blog/BlogApplication.java
+++ blog-service/src/main/java/com/chm/book/blog/BlogApplication.java
@@ -1,9 +1,12 @@
 package com.chm.book.blog;
 
+import com.chm.book.blog.datasource.DynamicDataSourceRegister;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({DynamicDataSourceRegister.class})
 public class BlogApplication {
 
 	public static void main(String[] args) {
