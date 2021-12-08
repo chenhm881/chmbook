3a6cb6e04a017a6441053ed23e0ba534f18df202
huiming chen <huchen@expedia.com>
2021-12-08 20:05:02 +0800

    save article tag

--- article-service/src/main/java/com/chm/book/article/service/ArticleTagService.java
+++ article-service/src/main/java/com/chm/book/article/service/ArticleTagService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ArticleTagService {
@@ -27,38 +27,50 @@ public class ArticleTagService {
         List<ArticleTag> articleTagList = findAll(articleId);
 
         List<ArticleTag> insertArticleTagList = new ArrayList<>();
-        List<ArticleTag> updateArticleTagList = new ArrayList<>();
+        List<ArticleTag> noUpdateArticleTagList = new ArrayList<>();
+        List<ArticleTag> addUpdateArticleTagList = new ArrayList<>();
+        List<Integer> insertTagList = new ArrayList<>();
 
         for(Integer tagId : tags) {
-            ArticleTag findArticleTag = articleTagList.stream().filter(item -> item.getTagId().equals(tagId)).findFirst().orElse(null);
-            if (findArticleTag != null) {
-                for (ArticleTag articleTag : articleTagList) {
-                    if (findArticleTag.getTagId().equals(articleTag.getTagId())) {
-                        if (!articleTag.isStatus()) {
-                            articleTag.setStatus(true);
-                            updateArticleTagList.add(articleTag);
-                        }
-                    } else if (articleTag.isStatus()) {
-                        articleTag.setStatus(false);
-                        updateArticleTagList.add(articleTag);
+            boolean isTag = false;
+            for (ArticleTag articleTag : articleTagList) {
+                if (articleTag.getTagId().equals(tagId)) {
+                    isTag = true;
+                    noUpdateArticleTagList.add(articleTag);
+                    if (!articleTag.isStatus()) {
+                        articleTag.setStatus(true);
+                        addUpdateArticleTagList.add(articleTag);
                     }
                 }
-
-            } else {
-                ArticleTag articleTag = new ArticleTag();
-                articleTag.setArticleId(articleId);
-                articleTag.setTagId(tagId);
-                articleTag.setStatus(true);
-                insertArticleTagList.add(articleTag);
+            }
+            if (!isTag) {
+                insertTagList.add(tagId);
             }
         }
 
-       if(insertArticleTagList.size() > 0) {
+        articleTagList.removeAll(noUpdateArticleTagList);
+
+        articleTagList = articleTagList.stream().filter(articleTag -> articleTag.isStatus()).map(articleTag -> {
+            articleTag.setStatus(false);
+            return articleTag;
+        }).collect(Collectors.toList());
+
+        articleTagList.addAll(addUpdateArticleTagList);
+
+        insertTagList.forEach(tagId -> {
+            ArticleTag articleTag = new ArticleTag();
+            articleTag.setArticleId(articleId);
+            articleTag.setTagId(tagId);
+            articleTag.setStatus(true);
+            insertArticleTagList.add(articleTag);
+        });
+
+        if(insertArticleTagList.size() > 0) {
            articleTagMapper.insertList(insertArticleTagList);
-       }
-       if(updateArticleTagList.size() > 0) {
-           articleTagMapper.updateList(updateArticleTagList, articleId);
-       }
+        }
+        if(articleTagList.size() > 0) {
+           articleTagMapper.updateList(articleTagList, articleId);
+        }
         return 1;
     }
 
